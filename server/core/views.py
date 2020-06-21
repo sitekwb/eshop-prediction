b8aab6ec8df51d6050809bbb8a5938e982a97455
Maciej Kasprzyk <maciejkasprzyk98@gmail.com>
2020-06-21 20:35:47 +0200

    conversion view ready

--- server/core/views.py
+++ server/core/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from core.models import Conversion
+
+
+# class ConversionSerializer(serializers.Serializer):
+#     value = serializers.IntegerField()
+#     model_id = serializers.IntegerField()
+#
+#     def create(self, validated_data):
+#         return Conversion(**validated_data)
+#
+
+class ConversionSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Conversion
+
+
+# noinspection PyAbstractClass
+class UserIdSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
+
+
+# noinspection PyMethodMayBeStatic
+class ConversionView(APIView):
+
+    def post(self, request, *args, **kwargs):
+        UserIdSerializer(data=request.data).is_valid(raise_exception=True)
+
+        model_id = request.data['user_id'] % 2
+        data = {'value': request.data['value'], 'model_id': model_id}
+        serializer = ConversionSerializer(data=data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
